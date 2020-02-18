b0b52dda4e0d26378f59686e764c9546f2a36a74
campeasy <miciuddo@gmail.com>
2020-02-18 10:51:38 +0100

    Add command line launch support

--- src/main.py
+++ src/main.py
@@ -7,8 +7,47 @@
 #   Artificial Intelligence Laboratory
 #
 
+import sys
+
 from Game import Game
 
+def check_input(input_list):
+    correct_call = "\n[ERROR] correct call:   python main.py WIDTH HEIGHT [S/M/L]\n"
+
+    # Input size check:
+    if(len(input_list) < 3):
+        print(correct_call)
+        return False
+
+    # Width check:
+    if(not (input_list[1].isdigit())):
+        print(correct_call)
+        return False
+
+    # Height check:
+    if(not (input_list[2].isdigit())):
+        print(correct_call)
+        return False
+
+    # Grid size check:
+    if(not (input_list[3].isascii())):
+        print(correct_call)
+        return False
+
+    grid_size = input_list[3]
+    if(not (grid_size == "L" or grid_size == "M" or grid_size == "S")):
+        print(correct_call)
+        return False
+
+    return True
+
 if __name__ == "__main__":
-    g = Game("a_star_pathfinding", 1700, 950, "M")
+    if(not (check_input(sys.argv))):
+        sys.exit(1)
+
+    width = int(sys.argv[1])
+    height = int(sys.argv[2])
+    grid_size = sys.argv[3]
+
+    g = Game("a_star_pathfinding", width, height, grid_size)
     g.run()
