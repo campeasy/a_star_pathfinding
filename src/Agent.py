75b73fb65648763298f597d00a6e59744d79b45d
campeasy <miciuddo@gmail.com>
2020-02-19 10:58:32 +0100

    Made refactoring and documentation in Agent's A-star algorithm for educational purposes

--- src/Agent.py
+++ src/Agent.py
@@ -34,50 +34,67 @@ class Agent():
         return np.sqrt((b[0] - a[0]) ** 2 + (b[1] - a[1]) ** 2)
 
     def __a_star(self, labth, start, goal):
-        neighbors = [(0,1),(0,-1),(1,0),(-1,0),(1,1),(1,-1),(-1,1),(-1,-1)]
-        close_set = set()
-        came_from = {}
+        neighbors_list = [(0,1), (0,-1), (1,0), (-1,0), (1,1), (1,-1), (-1,1), (-1,-1)]
 
         # Dictionaries with the g and f score for each node:
         gscore = {start:0}
         fscore = {start:self.__heuristic(start, goal)}
 
-        oheap = []
-        heapq.heappush(oheap, (fscore[start], start))
+        # Dictionary for storing the parent for each node in the path from start to goal:
+        came_from = {}
 
-        while oheap:
-            current = heapq.heappop(oheap)[1]
+        open_list = []
+        close_set = set()
 
-            if current == goal:
-                data = []
-                while current in came_from:
-                    data.append(current)
-                    current = came_from[current]
-                return data
+        heapq.heappush(open_list, (fscore[start], start) )
 
-            close_set.add(current)
+        while(open_list):
 
-            for i, j in neighbors:
-                neighbor = current[0] + i, current[1] + j
-                tentative_g_score = gscore[current] + self.__heuristic(current, neighbor)
+            # Popping the node with the lowest f score from the open list:
+            current_node = heapq.heappop(open_list)[1]
 
-                if 0 <= neighbor[0] < labth.shape[0]:
-                    if 0 <= neighbor[1] < labth.shape[1]:
-                        if labth[neighbor[0]][neighbor[1]] == 1:
+            # Goal node is found:
+            if(current_node == goal):
+
+                # Generating the path:
+                path_from_start_to_goal = []
+                while(current_node in came_from):
+                    path_from_start_to_goal.append(current_node)
+                    current_node = came_from[current_node]
+
+                return path_from_start_to_goal
+
+            # If the current node is not the goal, add it to the close set:
+            close_set.add(current_node)
+
+            # Examining each neighbor of the current node:
+            for i, j in neighbors_list:
+
+                neighbor = current_node[0] + i, current_node[1] + j
+                tentative_g_score = gscore[current_node] + self.__heuristic(current_node, neighbor)
+
+                # Skip this iteration if neighbor is an obstacle or if is not inside the grid:
+                if(0 <= neighbor[0] < labth.shape[0]):
+                    if(0 <= neighbor[1] < labth.shape[1]):
+                        if(labth[neighbor[0]][neighbor[1]] == 1):
                             continue
                     else:
                         continue
                 else:
                     continue
 
-                if neighbor in close_set and tentative_g_score >= gscore.get(neighbor, 0):
+                # Skip this iteration if the neighbor has already been visited AND is not possible to improve the g score:
+                if(neighbor in close_set and tentative_g_score >= gscore.get(neighbor, 0)):
                     continue
 
-                if tentative_g_score < gscore.get(neighbor, 0) or neighbor not in [i[1] for i in oheap]:
-                    came_from[neighbor] = current
+                # If is it possible to improve the g score OR the neighbor is not already in the open list:
+                if(tentative_g_score < gscore.get(neighbor, 0) or neighbor not in [i[1] for i in open_list]):
+                    came_from[neighbor] = current_node
                     gscore[neighbor] = tentative_g_score
                     fscore[neighbor] = tentative_g_score + self.__heuristic(neighbor, goal)
-                    heapq.heappush(oheap, (fscore[neighbor], neighbor))
+                    heapq.heappush(open_list, (fscore[neighbor], neighbor))
+
+        # There is no path from start to goal:
         return False
 
     def findpath_to_goal(self):
